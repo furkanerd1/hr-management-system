6b4255736531fa2b3b91288d42859f2d3c6df6ef
furkanerdogan <furkanerdd1@gmail.com>
2025-08-13 13:54:05 +0300

    feat: add ApiPaths constants and DepartmentController with CRUD methods

---
+++ src/main/java/com/furkanerd/hr_management_system/config/ApiPaths.java
@@ -0,0 +1,10 @@
+package com.furkanerd.hr_management_system.config;
+
+public class ApiPaths {
+
+    public static final String API_VERSION= "/api/v1";
+
+    // DEPARTMENTS
+    public static final String DEPARTMENTS= API_VERSION+"/departments";
+    public static final String DEPARTMENTS_BY_ID= API_VERSION+"/departments/{id}";
+}
---
+++ src/main/java/com/furkanerd/hr_management_system/controller/DepartmentController.java
@@ -0,0 +1,55 @@
+package com.furkanerd.hr_management_system.controller;
+
+import com.furkanerd.hr_management_system.model.dto.request.department.DepartmentCreateRequest;
+import com.furkanerd.hr_management_system.model.dto.request.department.DepartmentUpdateRequest;
+import com.furkanerd.hr_management_system.model.dto.response.department.DepartmentDetailResponse;
+import com.furkanerd.hr_management_system.model.dto.response.department.ListDepartmentResponse;
+import com.furkanerd.hr_management_system.service.DepartmentService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.furkanerd.hr_management_system.config.ApiPaths.DEPARTMENTS;
+import static com.furkanerd.hr_management_system.config.ApiPaths.DEPARTMENTS_BY_ID;
+
+@RestController
+@RequestMapping(DEPARTMENTS)
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    //TODO : Role based ( HR, MANAGER)
+    public ResponseEntity<List<ListDepartmentResponse>> getAllDepartments() {
+        return ResponseEntity.ok(departmentService.listAllDepartments());
+    }
+
+    @PostMapping
+    //TODO : Role based ( HR)
+    public ResponseEntity<DepartmentDetailResponse> createDepartment(
+            @Valid  @RequestBody DepartmentCreateRequest departmentCreateRequest){
+        return  ResponseEntity.ok(departmentService.createDepartment(departmentCreateRequest));
+    }
+
+    @PutMapping(DEPARTMENTS_BY_ID)
+    //TODO : Role based ( HR)
+    public ResponseEntity<DepartmentDetailResponse> updateDepartment(
+            @PathVariable("id") UUID departmentId ,
+            @Valid @RequestBody DepartmentUpdateRequest departmentUpdateRequest){
+        return ResponseEntity.ok(departmentService.updateDepartment(departmentId, departmentUpdateRequest));
+    }
+
+    @DeleteMapping(DEPARTMENTS_BY_ID)
+    public ResponseEntity<Void> deleteDepartment(
+            @PathVariable("id") UUID departmentId){
+        departmentService.deleteDepartment(departmentId);
+        return ResponseEntity.ok().build();
+    }
+}
