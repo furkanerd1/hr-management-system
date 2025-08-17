49d001e6bae6720904f83f2b375422898e3f7de3
furkanerdogan <furkanerdd1@gmail.com>
2025-08-17 16:42:41 +0300

    feat: add RoleHierarchy bean for hierarchical role handling

--- src/main/java/com/furkanerd/hr_management_system/config/SecurityConfig.java
+++ src/main/java/com/furkanerd/hr_management_system/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import com.furkanerd.hr_management_system.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -39,6 +41,14 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_HR > ROLE_MANAGER > ROLE_EMPLOYEE";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
     }
 
     @Bean
